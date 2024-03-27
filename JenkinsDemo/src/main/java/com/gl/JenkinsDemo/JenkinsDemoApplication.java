a8ee3e6bcb9ab89eafd339fa78c145b88aaf32a2
syedsaqib516 <140151865+syedsaqib516@users.noreply.github.com>
2024-03-27 10:40:12 +0530

    Add files via upload

---
+++ JenkinsDemo/src/main/java/com/gl/JenkinsDemo/JenkinsDemoApplication.java
@@ -0,0 +1,15 @@
+package com.gl.JenkinsDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsDemoApplication {
+
+	
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsDemoApplication.class, args);
+		System.out.println("Hello World");
+	}
+
+}
